fc2c3f432cc73b39d71ae16f1e66319f66067e5a
Diego Batista Pereira <diegobpcelular@gmail.com>
2023-10-13 18:42:41 -0300

    feat: boilerplate

---
+++ src/main/java/com/prog/keycloak/KeycloakApplication.java
@@ -0,0 +1,41 @@
+package com.prog.keycloak;
+
+import com.prog.keycloak.entity.Employee;
+import com.prog.keycloak.service.EmployeeService;
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/employees")
+public class KeycloakApplication {
+
+	@Autowired
+	private EmployeeService service;
+
+	@GetMapping("/{employeeId}")
+	@RolesAllowed("user")
+	public ResponseEntity<Employee> getEmployee(@PathVariable int employeeId) {
+		return ResponseEntity.ok(service.getEmployee(employeeId));
+	}
+
+	@GetMapping
+	@RolesAllowed("admin")
+	public ResponseEntity<List<Employee>> findALlEmployees() {
+		return ResponseEntity.ok(service.getAllEmployees());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakApplication.class, args);
+	}
+
+}
